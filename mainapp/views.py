1edbf50c69dc87e549bef1ab4000fd5b327f735f
Paule Kozik <kpasha11@mail.ru>
2022-09-23 01:20:47 +0300

    'firs'

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def hello_view(request):
-    return (HttpResponse("Hello world"))
+class MainPageView(TemplateView):
+    template_name = 'index.html'
+
+
+class ContactsPageView(TemplateView):
+    template_name = 'contacts.html'
+
+
+class CoursesPageView(TemplateView):
+    template_name = 'courses_list.html'
+
+
+class DocSitePageView(TemplateView):
+    template_name = 'doc_site.html'
+
+
+class LoginPageView(TemplateView):
+    template_name = 'login.html'
+
+
+class NewsPageView(TemplateView):
+    template_name = 'news.html'
