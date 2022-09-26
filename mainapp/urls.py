82aea84313eb300ae14d652d0eff5de32942c267
Paule Kozik <kpasha11@mail.ru>
2022-09-27 00:26:42 +0300

    second_commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import *
 
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
 urlpatterns = [
-    path('', MainPageView.as_view()),
-    path('contacts/', ContactsPageView.as_view()),
-    path('courses_list/', CoursesPageView.as_view()),
-    path('doc_site/', DocSitePageView.as_view()),
-    path('login/', LoginPageView.as_view()),
-    path('news/', NewsPageView.as_view()),
+    path('', MainPageView.as_view(), name='main'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('courses_list/', CoursesPageView.as_view(), name='courses'),
+    path('doc_site/', DocSitePageView.as_view(), name='docs'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('news/', NewsPageView.as_view(), name='news'),
 ]
