c6385739fa6d1036be371db4cadacd869a669040
Paule Kozik <kpasha11@mail.ru>
2022-10-08 13:46:41 +0300

    finish lesson_6 commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from mainapp.manadgers.news_manager import NewsManager
 
 
 class News(models.Model):
+    object = NewsManager()
     title = models.CharField(max_length=256, verbose_name="Title")
     preambule = models.CharField(max_length=1024, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
@@ -47,6 +50,19 @@ class Courses(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+
+class CourseFeedback(models.Model):
+    RATING = ((5, "⭐⭐⭐⭐⭐"), (4, "⭐⭐⭐⭐"), (3, "⭐⭐⭐"), (2, "⭐⭐"), (1, "⭐"))
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, verbose_name=_("Course"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("User"))
+    feedback = models.TextField(default=_("No feedback"), verbose_name=_("Feedback"))
+    rating = models.SmallIntegerField(choices=RATING, default=5, verbose_name=_("Rating"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.course} ({self.user})"
 
 
 class Lesson(models.Model):
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
+
+from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
 from datetime import datetime
 from django.core.paginator import Paginator
@@ -10,32 +16,8 @@ from .models import News
 class MainPageView(TemplateView):
     template_name = 'index.html'
 
-
-class ContactsPageView(TemplateView):
-    template_name = 'contacts.html'
-
-
-class CoursesPageView(TemplateView):
-    template_name = 'courses_list.html'
-
-
-class DocSitePageView(TemplateView):
-    template_name = 'doc_site.html'
-
-
-class LoginPageView(TemplateView):
-    template_name = 'login.html'
-
-class NewsPageDetailView(TemplateView):
-    template_name = "mainapp/news_detail.html"
-
-    def get_context_data(self, pk=None, **kwargs):
-        context = super().get_context_data(pk=pk, **kwargs)
-        context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
-        return context
-
-class NewsPageView(TemplateView):
-    template_name = 'news.html'
+class NewsPageView(ListView):
+    model = mainapp_models.News
     paginated_by = 3
 
     def get_context_data(self, **kwargs):
@@ -51,3 +33,73 @@ class NewsPageView(TemplateView):
         context['page'] = page
 
         return context
+
+class NewsCreateView(PermissionRequiredMixin, CreateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.add_news",)
+
+
+class NewsDetailView(DetailView):
+    model = mainapp_models.News
+
+
+class NewsUpdateView(PermissionRequiredMixin, UpdateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.change_news",)
+
+
+class NewsDeleteView(PermissionRequiredMixin, DeleteView):
+    model = mainapp_models.News
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.delete_news",)
+
+class CoursesPageView(TemplateView):
+    template_name = 'courses_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CoursesPageView, self).get_context_data(**kwargs)
+        context["objects"] = mainapp_models.Courses.objects.all()[:7]
+        return context
+
+class CoursesDetailView(TemplateView):
+    template_name = "courses_detail.html"
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super(CoursesDetailView, self).get_context_data(**kwargs)
+        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
+        if not self.request.user.is_anonymous:
+            if not mainapp_models.CourseFeedback.objects.filter(
+                    course=context["course_object"], user=self.request.user
+            ).count():
+                context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
+                    course=context["course_object"], user=self.request.user
+                )
+        context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
+            course=context["course_object"]
+        ).order_by("-created", "-rating")[:5]
+        return context
+
+class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
+    model = mainapp_models.CourseFeedback
+    form_class = mainapp_forms.CourseFeedbackForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+        rendered_card = render_to_string("mainapp/includes/feedback_card.html", context={"item": self.object})
+        return JsonResponse({"card": rendered_card})
+
+
+class LoginPageView(TemplateView):
+    template_name = 'login.html'
+
+class ContactsPageView(TemplateView):
+    template_name = 'contacts.html'
+
+class DocSitePageView(TemplateView):
+    template_name = 'doc_site.html'
